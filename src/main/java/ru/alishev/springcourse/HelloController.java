57f06b18e77019502def0a48916aaae6d80b258f
Sagid Kadirov <sagidkadirov1999@gmail.com>
2024-11-04 16:29:45 +0500

    from thymeleaf-spring5 to thymeleaf-spring6

--- src/main/java/ru/alishev/springcourse/HelloController.java
+++ src/main/java/ru/alishev/springcourse/HelloController.java
@@ -1,12 +1,15 @@
-package uz.kadirov.springcourse;
+package ru.alishev.springcourse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
-public class HelloController {
+public class HelloController{
+
     @GetMapping("/hello-world")
     public String sayHello(){
-        return "hello-world";
+        System.out.println("422231231231");
+        return "hello_world";
     }
 }
