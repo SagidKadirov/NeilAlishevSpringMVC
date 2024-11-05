ca4bd259d509d00490f1189e6d1630a32b82185b
Sagid Kadirov <sagidkadirov1999@gmail.com>
2024-11-05 17:15:02 +0500

    using HttpServletRequest for request.getParameter in FirstController class

---
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -0,0 +1,26 @@
+package ru.alishev.springcourse.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        System.out.println("Обращение на hello\nПрием параметров\nИмя: "+name+"\nФамилия: "+surname);
+        return "first/hello";
+    }
+    @GetMapping("/goodbye")
+    public String goodByePage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        System.out.println("Обращение на goodbye\nПрием параметров\nИмя: "+name+"\nФамилия: "+surname);
+        return "first/goodbye";
+    }
+}
