5e751489a979a980dd3af803277fe0b6114640ec
Sagid Kadirov <sagidkadirov1999@gmail.com>
2024-11-05 17:22:00 +0500

    using @RequestParam instead of HttpServletRequest in firstController class in hello and goodbye methods

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -1,26 +1,29 @@
 package ru.alishev.springcourse.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request){
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam("name")String name,
+                            @RequestParam("surname") String surname){
+
         System.out.println("Обращение на hello\nПрием параметров\nИмя: "+name+"\nФамилия: "+surname);
         return "first/hello";
+
     }
+
     @GetMapping("/goodbye")
-    public String goodByePage(HttpServletRequest request){
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String goodByePage(@RequestParam("name")String name,
+                              @RequestParam("surname") String surname){
+
         System.out.println("Обращение на goodbye\nПрием параметров\nИмя: "+name+"\nФамилия: "+surname);
         return "first/goodbye";
+
     }
 }
