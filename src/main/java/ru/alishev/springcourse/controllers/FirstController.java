959e657b562e3996fd31676782465fd9dfdcbe98
Sagid Kadirov <sagidkadirov1999@gmail.com>
2024-11-06 14:48:00 +0500

    page calculator and method in controller class created

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -27,6 +27,45 @@ public class FirstController {
         model.addAttribute("message", "имя: " + name + ", фамилия: " + surname);
         System.out.println("Обращение на goodbye\nПрием параметров\nИмя: " + name + "\nФамилия: " + surname);
         return "first/goodbye";
+    }
 
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam(value= "a",required = false) Integer a,
+                                 @RequestParam(value = "b", required = false) Integer b,
+                                 @RequestParam(value = "action",required = false) String action,
+                                 Model model){
+        if(a!=null&&b!=null&&action!=null)
+        {
+            double c;
+        switch (action)
+        {
+            case "addition":
+                c=a+b;
+                model.addAttribute("result", "The result of addition number = "+ a+" and number = "+b+" is "+c);
+                break;
+            case "subtraction":
+                c=a-b;
+                model.addAttribute("result", "The result of subtraction number = "+ a+" and number = "+b+" is "+c);
+                break;
+            case "multiplication":
+                c=a*b;
+                model.addAttribute("result", "The result of multiplication number = "+ a+" and number = "+b+" is "+c);
+                break;
+            case "division":
+                if(b!=0){
+                    c=a/(double)b;
+                    model.addAttribute("result", "The result of division number = "+ a+" and number = "+b+" is "+c);
+                }
+                else {
+                    model.addAttribute("result", "Can't divide by zero");
+                }
+                break;
+            default:
+                model.addAttribute("result", "Error the calculator don't understand your wishes");
+        }
+        }
+
+
+        return "first/calculator";
     }
 }
