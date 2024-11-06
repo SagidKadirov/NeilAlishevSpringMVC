73665453d60815c96eedc0460cb42993457400e2
Sagid Kadirov <sagidkadirov1999@gmail.com>
2024-11-06 08:04:58 +0500

    add new references to html files (with parameters)

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,19 +11,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value="name", required = false)String name,
-                            @RequestParam(value = "surname", required = false) String surname){
-
-        System.out.println("Обращение на hello\nПрием параметров\nИмя: "+name+"\nФамилия: "+surname);
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message", "name: " + name + "surname: " + surname);
+        System.out.println("Обращение на hello\nПрием параметров\nИмя: " + name + "\nФамилия: " + surname);
         return "first/hello";
 
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage(@RequestParam("name")String name,
-                              @RequestParam("surname") String surname){
-
-        System.out.println("Обращение на goodbye\nПрием параметров\nИмя: "+name+"\nФамилия: "+surname);
+    public String goodByePage(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "surname", required = false) String surname,
+                              Model model) {
+        model.addAttribute("message", "name: " + name + "surname: " + surname);
+        System.out.println("Обращение на goodbye\nПрием параметров\nИмя: " + name + "\nФамилия: " + surname);
         return "first/goodbye";
 
     }
